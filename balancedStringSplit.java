2a8a9c0d723a2c5b4f957e72c9696e6c47d630c4
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-09-28 13:22:00 +0530

    Create balancedStringSplit.java
    Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
    
    Given a balanced string s, split it into some number of substrings such that:
    
    Each substring is balanced.
    Return the maximum number of balanced strings you can obtain.

---
+++ balancedStringSplit.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int n=s.length();
+        int count1=0;
+        int count2=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='L'){
+                count1++;
+            }
+            else if(s.charAt(i)=='R'){
+                count2++;
+            }
+            if(count1==count2){
+                count++;
+            }
+        }
+        return count;
+    }
+}
